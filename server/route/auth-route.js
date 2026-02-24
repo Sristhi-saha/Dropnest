ecf6c877a2e0f38c26112bbee105b03d581998fd
Sristhi-saha <shyamalsaha558@gmail.com>
2026-02-24 11:41:33 +0530

    middelware creation done

--- server/route/auth-route.js
+++ server/route/auth-route.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const {authMiddelware} = require('../middelware/auth')
 const {logoutUser,loginUSer,registerUser,resetPassword,sendrestPasswordOtp} = require('../controllers/auth-controller');
 
 const authRouter = express.Router();
 
 authRouter.post('/register',registerUser);
 authRouter.post('/login',loginUSer);
-authRouter.post('/resetPassword',resetPassword);
-authRouter.post('/sendResetPasswordOtp',sendrestPasswordOtp);
+authRouter.post('/resetPassword',authMiddelware,resetPassword);
+authRouter.post('/sendResetPasswordOtp',authMiddelware,sendrestPasswordOtp);
 authRouter.post('/logout',logoutUser);
 
 module.exports=authRouter;
--- server/route/file-upload-route.js
+++ server/route/file-upload-route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const upload = require('../middelware/multer'); 
+const {authMiddelware} = require('../middelware/auth')
 const { uploadFile, deleteFile, fetchAllFiles } = require('../controllers/fil-upload-controller');
 
 const fileRouter = express.Router();
 
-fileRouter.post('/upload-file', upload.single("file"), uploadFile);
-fileRouter.delete('/delete-file/:id', deleteFile);
+fileRouter.post('/upload-file',authMiddelware, upload.single("file"), uploadFile);
+fileRouter.delete('/delete-file/:id',authMiddelware, deleteFile);
 fileRouter.get('/fetch-all-files',fetchAllFiles)
 
 module.exports = fileRouter;
